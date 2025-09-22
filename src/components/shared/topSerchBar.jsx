a91bdc38069a3d5385b7330e8298eb6f8e3041de
ABid hasan <abeydhasan134@gmail.com>
2025-09-22 23:31:06 +0600

    nav setting

---
+++ src/components/shared/topSerchBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+// import vastOraLogo from '../../../public/image/mainlogo.png'
+
+const TopSerchBar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <img src='image/logo.png' alt="vastora sports" className='h-[90px] w-[110px]'/>
+    {/* <p className='text-5xl'>Vastora</p> */}
+  </div>
+  <div className="flex gap-2">
+    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src='#' />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default TopSerchBar
