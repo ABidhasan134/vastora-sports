108e9ee7652bfdaa91889d958568237f26325dfd
ABid hasan <abeydhasan134@gmail.com>
2025-09-24 01:26:03 +0600

    navbar test

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* <div className="container mx-auto"> */}
+
         <TopSerchBar></TopSerchBar>
+        {/* </div> */}
         {children}
       </body>
     </html>
--- src/components/shared/topSerchBar.jsx
+++ src/components/shared/topSerchBar.jsx
@@ -1,38 +1,94 @@
+import Link from 'next/link'
 import React from 'react'
 // import vastOraLogo from '../../../public/image/mainlogo.png'
 
 const TopSerchBar = () => {
+  const navItemList=[
+    {
+      url: '/newlatest',
+      title: 'New & Trending',
+    },
+    {
+      url: '/player',
+      title: 'Player',
+    },
+    {
+      url: '/fan',
+      title: 'Fan',
+    },
+    {
+      url: '/retro',
+      title: 'Retro'
+    }
+  ]
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <img src='image/logo.png' alt="vastora sports" className='h-[90px] w-[110px]'/>
-    {/* <p className='text-5xl'>Vastora</p> */}
-  </div>
-  <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src='#' />
+   <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <img src='image/mainlogo.png' alt="Vastora sports" />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
         </div>
+        <button>
+          <img src="image/logo.png" alt="Vastora sports" className='w-[70px] h-[60px]' />
+        </button>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {/* <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li> */}
+              {/* </ul>
+            </details>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li> */}
+          {
+            navItemList.map((item,index)=>{
+              return <li key={index}>
+                <Link href={item.url}>{item.title}</Link>
+              </li>
+            })
+          }
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
     </div>
-  </div>
-</div>
   )
 }
 
